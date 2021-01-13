2a2f24e47df4ab0acfe9564fdfde8f67dd7d3894
SachaG <358832+SachaG@users.noreply.github.com>
2021-01-13 11:01:32 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from "react"
 // import ResultsComingSoon from "../components/ResultsComingSoon"
 import Share from "../components/Share"
 import Newsletter from "../components/Newsletter"
-import TakeSurvey from "../components/TakeSurvey"
+// import TakeSurvey from "../components/TakeSurvey"
 import About from "../components/About"
 import Footer from "../components/Footer"
 import Intro from "../components/Intro"
 import Languages from "../components/Languages"
 import Translators from "../components/Translators"
 import Logo from "../components/Logo"
-// import ViewResults from "../components/ViewResults"
+import ViewResults from "../components/ViewResults"
 // import ReactGA from 'react-ga'
 import Layout from "../layouts/layout.js"
 
@@ -21,8 +21,8 @@ const Index = () => (
       <Intro />
       {/* <ResultsComingSoon /> */}
       <div className="Block Actions Block--Actions">
-        <TakeSurvey />
-        {/* <ViewResults /> */}
+        {/* <TakeSurvey /> */}
+        <ViewResults />
       </div>
       <Share />
       <Newsletter />
